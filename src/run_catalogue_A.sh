18a5e34cca070376f70c9a439bbe5f07145b4f63
Aayush Gupta <aayushgupta@umass.edu>
2020-04-23 01:04:42 -0400

    Modified scripts

--- src/run_catalogue_A.sh
+++ src/run_catalogue_A.sh
@@ -1,5 +1,5 @@
 source venv/bin/activate &&
-pip3 install -r requirements.txt --user&&
+pip3 install -r requirements.txt --user &&
 python3 set_config.py 1 &&
 python3 catalogue/catalog_A/catalogue.py 34602 'sqlite:///catalog_A.db' 34612 'catalogue/catalog_A/catalog_A_log.txt'
 	
